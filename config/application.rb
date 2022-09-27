fbb37740b428b09deda859fec24d37847e2fe548
Jai <jai@tecorb.co>
2022-09-27 13:22:09 +0530

    configuration.yml file setup to load ENV variables

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module Auth2Factor
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'configuration.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end if File.exists?(env_file)
+    end
   end
 end
