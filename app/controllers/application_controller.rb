70a3e081f36c15b3dd6b7655c1d782652fce54a9
Jai <jai@tecorb.co>
2022-10-06 19:05:42 +0530

    Devise redirection after signup, login and dashboard integerated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
   end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+   	root_path
+  end
 end
