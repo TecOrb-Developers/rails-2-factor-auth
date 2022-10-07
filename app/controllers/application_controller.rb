dd63c019d1584e51d13412022714bde1f4730ff4
Jai <jai@tecorb.co>
2022-10-07 17:47:35 +0530

    Two factor login settings integrated with sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  include ApplicationHelper
+  
   protected
 
   def configure_permitted_parameters
