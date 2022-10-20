7041e628e5ad8f38ffb79cc93138fd631865c669
Jai Rajput <47137013+developerJai@users.noreply.github.com>
2022-10-20 20:18:23 +0530

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -33,5 +33,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_134121) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
